bbd79cfbd621cfec906ee6a14d3c077f1a536e1f
shallon kobusinge <kobshallon041@gmail.com>
2022-01-04 11:10:38 +0200

    [FEAT] applying action interfaces

--- src/store/reducer/repositoriesReducer.ts
+++ src/store/reducer/repositoriesReducer.ts
@@ -5,11 +5,28 @@ interface RepositoriesInitialState  {
     data: string[]
 }
 
-interface Action{
-    type: string,
-    payload: any,
+interface SearchRepositoriesAction {
+    type: ActionTypes.SEARCH_REPOSITORIES,
+    
+}
+interface SearchRepositoriesSuccessAction{
+    type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+    payload: string[]
+
 }
 
+interface SearchRepositoriesFailureAction{
+    type: ActionTypes.SEARCH_REPOSITORIES_FAILURE,
+    payload: string
+}
+
+type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesFailureAction;
+
+enum ActionTypes {
+    SEARCH_REPOSITORIES = "searchRepositories",
+    SEARCH_REPOSITORIES_SUCCESS = "searchRepositories_success",
+    SEARCH_REPOSITORIES_FAILURE = "searchRepositories_failure"
+}
 
 const reducer = (state : RepositoriesInitialState, action: Action) : RepositoriesInitialState => {
     switch (action.type) {
@@ -18,7 +35,7 @@ const reducer = (state : RepositoriesInitialState, action: Action) : Repositorie
         case "searchRepositories_success":
             return{loading: false, error: null, data:action.payload}
         case "searchRepositories_failure":
-            return{loading:false,error: action.payload,data:[]}
+            return{loading:false,error: null,data:[]}
         default:
             return state;
     }

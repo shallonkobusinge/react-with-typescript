098fe65997eb253a8102aa32848f1620b0aecbd2
shallon kobusinge <kobshallon041@gmail.com>
2022-01-05 21:16:03 +0200

    [FEAT] selecting state in typescript

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
 
 const RepositoriesList: React.FC = () => {
     const [names, setNames] = React.useState('');
     const { searchResults } = useActions();
+    const { data, error, loading } = useSelector((state: any) => state.repository);
+
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -16,6 +19,11 @@ const RepositoriesList: React.FC = () => {
             <form onSubmit={onSubmit}>
                 <input type="text" value={names} onChange={(e) => setNames(e.target.value)}></input>
                 <button>Search</button>
+                {/* {data.map((item: string) => (
+                    <>
+                        <h2>{item}</h2>
+                    </>
+                ))} */}
             </form>
         </div>
     )
